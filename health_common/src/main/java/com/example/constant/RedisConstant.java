26bf5389b2f67d52b00a4a1d2889618a8e801fe3
dont-fret <15222007713@163.com>
2020-10-07 14:46:28 +0800

    提交测试

---
+++ health_common/src/main/java/com/example/constant/RedisConstant.java
@@ -0,0 +1,10 @@
+package com.example.constant;
+
+public class RedisConstant {
+    //套餐图片所有图片名称
+    public static final String SETMEAL_PIC_RESOURCES = "setmealPicResources";
+    //套餐图片保存在数据库中的图片名称
+    public static final String SETMEAL_PIC_DB_RESOURCES = "setmealPicDbResources";
+    //套餐删除和更新后被覆盖的旧图片的名称
+    public static final String SETMEAL_PIC_OLD_RESOURCES = "setmealPicOldResources";
+}
